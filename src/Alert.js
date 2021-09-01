be3380b958ae712c0af13cf03a3d49a390ab5c99
Chris <chris.ware.dev@gmail.com>
2021-09-01 15:41:19 -0700

    Complete

--- src/Alert.js
+++ src/Alert.js
@@ -1,18 +1,13 @@
 import React, { useEffect } from "react";
 
-const Alert = ({ type, msg, removeAlert }) => {
- useEffect(() => {
-     const timeout = setTimeout(()=> {
-         removeAlert()
-     }, 3000)
-     return () => clearTimeout(timeout)
- }, [])
-
-  return (
-    <div>
-      <p className={`alert alert-${type}`}>{msg}</p>
-    </div>
-  );
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
 export default Alert;
