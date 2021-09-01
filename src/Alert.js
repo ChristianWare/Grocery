c39ff370d08e0648702bf5c1f8cc73ab02d3d3a7
Chris <chris.ware.dev@gmail.com>
2021-09-01 14:20:43 -0700

    Initial Structuure setup/useState functionality

---
+++ src/Alert.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Alert = () => {
+  return (
+    <div>
+      <h2>Alert Component</h2>
+    </div>
+  );
+};
+
+export default Alert;
---
+++ src/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const Alert = () => {
+  return (
+    <div>
+      <h2>List Component</h2>
+    </div>
+  );
+};
+
+export default Alert;
